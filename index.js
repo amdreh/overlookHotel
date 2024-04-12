59209777b6b05010413892ac02273609bb063d56
amdreh <amdreh@gmail.com>
2024-04-12 18:20:28 -0300

    Arquivos excluídos e imagens acrescentadas.

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ document.addEventListener('DOMContentLoaded', function() {
     selected.addEventListener('change', function() {
         if (selected.value === 'Sim') {
             document.body.style.backgroundColor= 'black';
+            evilSubtitle();
+            evilText();
         } else {location.reload();}
     });
 });
+
+function evilSubtitle () {
+    const subtitle = document.getElementById('subtitle');
+    subtitle.innerText = "SUA FAMÍLIA JAMAIS RETORNARÁ";    
+}
+
+function evilText() {
+    const firstP = document.getElementById('firstP');
+    const secondP = document.getElementById('secondP');
+
+    firstP.innerText = "ll boy all work and no play makes Jack a dull boy all work and no play makes Jack a dull boy all work and no play makes Jack a dull boy all work and no play makes Jack a dull boy all work and no play makes Jack a dull boy all work and no play makes Jack a dull boy all work and no play makes Jack a dull boy all work and no play makes Jack a dull boy all work and no play makes Jack a dull boy all work and no play makes Jack a dull boy all work and no play makes Jack a dull boy all work and no play makes Jack a dull boy ";
+    secondP.innerText = "and no play makes Jack a dull boy all work and no play makes Jack a dull boy all work and no play makes Jack a dull boy all work and no play makes Jack a dull boy all work and no play makes Jack a dull boy all work and no play makes Jack a dull boy ";
+}
