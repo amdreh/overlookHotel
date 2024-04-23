9164392a045b164cc17ebba8f30f5eb7f2511873
amdreh <amdreh@gmail.com>
2024-04-23 15:48:14 -0300

    Burrada

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
+
 document.addEventListener('DOMContentLoaded', function() {
+
+
     var selected = document.getElementById('powers');
     selected.addEventListener('change', function() {
         if (selected.value === 'Sim') {
@@ -37,3 +40,51 @@ function evilFootText() {
     const footText = document.getElementById('footText');
     footText.innerText = "redrumREDRUMredrum";
 }
+
+
+$('#cep').on('blur', function() {
+    let url1 = "https://viacep.com.br/ws/";
+    let url2 = $('#cep').val();
+    url2 = url2.replace(/\D/g,'');
+    let fullUrl = url1 + url2 + "/json/";
+
+    $.get(fullUrl, function(data){
+    var cepReturn = data;
+    $('#rua').val(cepReturn.logradouro);
+    $('#bairro').val(cepReturn.bairro);
+    $('#cidade').val(cepReturn.localidade);
+    $('#uf').val(cepReturn.uf)
+    }).fail(function(){
+        alert("ERRO");
+    })  
+});
+
+
+// $.get(fullUrl, function(data) {
+//     var cepReturn = data;
+//     alert(cepReturn.logradouro);
+//     // $('#rua').val(cepReturn.logradouro);
+// }).fail(function() {
+//     alert("Erro ao carregar o endereço. Por favor, verifique sua conexão de internet e tente novamente.");
+// });
+// });
+
+    
+
+
+// Fazendo uma requisição GET para a API
+// $.get('https://viacep.com.br/ws/28909250/json/', function(data) {
+//      Armazenando o retorno da API em uma variável
+//     var endereco = data;
+    
+//      Agora você pode acessar os dados do endereço assim:
+//     console.log(endereco.logradouro); // Exemplo de como acessar o logradouro
+// });
+
+// MÁSCARAS
+
+$(document).ready(function() {
+    $('#phone').mask('(00) 00000-0000');
+    $('#cpf').mask('000.000.000-00');
+    $('#cep').mask('00.000-000');
+});
